373c7121c72bb2df9e8783e80b561d57a94eab92
mihaicalinescu <m.calinescu18@gmail.com>
2023-04-05 17:39:38 +0300

    Very hard working on determining the lifespan

--- AndroidAppLifespan.py
+++ AndroidAppLifespan.py
@@ -1,8 +1,11 @@
 import re
 from pprint import pprint
+import time
+import datetime
 
 stored_data = []
 extracted_data = {}
+
 
 def parse_the_input_file():
     with open('logcat_applications.txt', 'rt') as myfile:
@@ -14,6 +17,7 @@ def parse_the_input_file():
             if 'Destroyed ActivityRecord' in myline:
                 stored_data.append(myline)
 
+
 def extract_specific_data():
     for line in stored_data:
         time = re.search(r'\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d{3}', line)
@@ -23,17 +27,30 @@ def extract_specific_data():
         start_date = time.group(0)
         # print (start_date)
         if time and package:
-            app = 'application{}'.format(len(extracted_data) + 1)
+            app = 'application_{}'.format(len(extracted_data) + 1)
             app_path = package.group(1)
             extracted_data[app] = {"app_path": app_path, "ts_app_started": start_date}
+            start_element = datetime.datetime.strptime(start_date, "%m-%d %H:%M:%S.%f")
+            start_timestamp = datetime.datetime.timestamp(start_element)
+            # print(start_timestamp)
+        stop_timestamp = 0
+        start_timestamp = 0
         if '/' in app_path:
             apps_value = app_path.split('/')[0]
             if apps_value and 'Destroyed ActivityRecord' in line:
                 stop_date = time.group(0)
                 extracted_data[app]['ts_app_closed'] = stop_date
+                stop_element = datetime.datetime.strptime(stop_date, "%m-%d %H:%M:%S.%f")
+                stop_timestamp = datetime.datetime.timestamp(stop_element)
+                # print(stop_timestamp)
 
+        extracted_data[app]['lifespan'] = stop_timestamp - start_timestamp
 
     pprint(extracted_data)
+    # for my_dict in extracted_data:
+    #     print(my_dict)
+
+
 if __name__ == "__main__":
     parse_the_input_file()
     extract_specific_data()
